798e71b51da39e486d6601e51188bc3334363a6e
sitesystem <l_aloaxa05@hotmail.com>
2022-04-04 20:04:10 -0500

    Primera actualización

---
+++ CURSO LÓGICA DE PROGRAMACIÓN PYTHON/HolaMundo.py
@@ -0,0 +1,7 @@
+# EJEMPLO SALUDO
+# COMENTARIOS: Sirven para documentar el programa
+# y explicar cada parte del código
+# A los comentarios los omite python y no los ejecuta
+
+print("HOLA JARED!!!")
+print(10-5)
